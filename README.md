cb93be80037e87b1058aebe4d1f4b008e2ba4953
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2016-04-06 16:57:29 +0200

    Added skeleton

--- README.md
+++ README.md
@@ -2,3 +2,15 @@
 
 Web site to perform a calculation of a single input using a single command line call inside a docker container.
 
+# Run
+
+```
+pip install -r requirements.txt
+cp settings.cfg-dist settings.cfg
+```
+
+Add Beehub credentials to settings.cfg and start web server with
+
+```
+python onebuttoncompute.py
+```
---
+++ onebuttoncompute.py
@@ -0,0 +1,34 @@
+import logging
+import tempfile
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+app.config.from_pyfile('settings.cfg')
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/compute', methods=['POST'])
+def compute():
+    logging.warn(request.form)
+    inputdir = tempfile.TemporaryDirectory('in')
+    outputdir = tempfile.TemporaryDirectory('out')
+
+    # Download input file from beehub to inputdir
+
+    # Run Docker
+
+    # Upload content of output dir to Beehub
+
+    inputdir.cleanup()
+    outputdir.cleanup()
+
+    result_url = flask.config['BEEHUB_ROOT'] + request.form['outputdir']
+
+    return render_template('result.html', result_url=result_url)
+
+def
+
+if __name__ == '__main__':
+    app.run()
