afdeab0d74346cbbbab165f02b5099dd767f0072
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2016-07-29 17:03:12 +0200

    UI:
    * Added input item type form radio group
    * Added workflow requirements help text
    
    Server:
    
    * Added recursive upload and download
    * Added placeholders for generating workflow wrapper and job order files

--- onebuttoncompute.py
+++ onebuttoncompute.py
@@ -46,18 +46,38 @@ class WebDAVClient(object):
     def upload(self, source, target):
         return self.client.upload(source, self.path + '/' + target)
 
-    def ls(self, path):
-        """List files/directories in path
+    def ls(self, path, recursive=False, relative=None):
+        """List files in path
 
         Args:
-            path (str):
+            path (str): The path
+            recursive (bool): Whether to do ls -R or ls
+            relative (str): The return files name should be relative to this, if None then set to path
 
         Returns:
-            list: File name or directory (ends with /) relative to path
+            list: File names
         """
         fpath = self.path + '/' + path
+        if relative is None:
+            relative = fpath + '/'
         listing = self.client.ls(fpath)
-        return [d.name.replace(fpath + '/', '') for d in listing if d.name != fpath + '/']
+        files = []
+        for item in listing:
+            if item.name == fpath + '/':
+                # skip self
+                continue
+            if item.name.endswith('/'):
+                # is directory
+                if recursive:
+                    files += self.ls(item.name.lstrip(self.path + '/').rstrip('/'), recursive=recursive, relative=fpath)
+            else:
+                files.append(item.name.lstrip(relative))
+
+        return files
+
+    def mkdirs(self, path):
+        fpath = self.path + '/' + path
+        self.client.mkdirs(fpath)
 
 
 class S3Client(object):
@@ -82,10 +102,14 @@ class S3Client(object):
         content_type = magic.from_file(source, mime=True)
         return self.client.fput_object(self.bucket, self.prefix + '/' + target, source, content_type)
 
-    def ls(self, path):
+    def ls(self, path, recursive=False):
         fpath = self.prefix + '/' + path + '/'
-        listing = self.client.list_objects(self.bucket, prefix=fpath)
+        listing = self.client.list_objects(self.bucket, prefix=fpath, recursive=recursive)
         return [d.object_name.replace(fpath, '') for d in listing]
+
+    def mkdirs(self, path):
+        # S3 has know directories
+        pass
 
 
 def remote_storage_client(config):
@@ -117,13 +141,14 @@ def index():
 def submit_job():
     data = request.get_json()
     remote_input_dir = data['inputdir'].rstrip('/')
+    input_item_type = data.get('inputitemtype', 'file')
     remote_workflow_file = data['cwl_workflow']
     remote_output_dir = data['outputdir'].rstrip('/')
-    if re.compile('[^a-zA-Z0-9-_.]').search(data['outputextension']):
+    outputextension = data.get('outputextension', '')
+    if re.compile('[^a-zA-Z0-9-_.]').search(outputextension):
         raise ValueError('outputextension can only contain alphanumeric or -_.')
-    output_extension = data['outputextension']
 
-    job = perform_computation.delay(remote_workflow_file, remote_input_dir, remote_output_dir, output_extension)
+    job = perform_computation.delay(remote_workflow_file, remote_input_dir, input_item_type, remote_output_dir, output_extension)
     return redirect("/jobs/{0}".format(job.id))
 
 
@@ -142,7 +167,7 @@ def status_job(job_id):
     return jsonify(response)
 
 
-def write_job_order(session_dir, input_dir, input_files, output_files, job_order_fn='job.yml'):
+def write_job_order_on_files(session_dir, input_dir, input_files, output_files, job_order_fn='job.yml'):
     abs_job_order_fn = session_dir + '/' + job_order_fn
     cwl_input_files = [{'class': 'File', 'path': input_dir + '/' + d} for d in input_files]
     data = {
@@ -157,7 +182,7 @@ def write_job_order(session_dir, input_dir, input_files, output_files, job_order
     return job_order_fn
 
 
-def write_workflow_wrapper(session_dir, workflow_file, workflow_wrapper_fn='workflow.wrapper.cwl'):
+def write_workflow_wrapper_on_files(session_dir, workflow_file, workflow_wrapper_fn='workflow.wrapper.cwl'):
     abs_workflow_wrapper_fn = session_dir + '/' + workflow_wrapper_fn
     wrapper = {
         'class': 'Workflow',
@@ -193,17 +218,43 @@ def write_workflow_wrapper(session_dir, workflow_file, workflow_wrapper_fn='work
 
 
 @celery.task(bind=True)
-def perform_computation(self, remote_workflow_file, remote_input_dir, remote_output_dir, output_extension=''):
+def perform_computation(self,
+                        remote_workflow_file,
+                        remote_input_dir,
+                        input_item_type,
+                        remote_output_dir,
+                        output_extension):
+    if input_item_type is 'file':
+        return perform_computation_on_files(self,
+                                     remote_workflow_file,
+                                     remote_input_dir,
+                                     remote_output_dir,
+                                     output_extension)
+    elif input_item_type is 'dir':
+        return perform_computation_on_dirs(self,
+                                    remote_workflow_file,
+                                    remote_input_dir,
+                                    remote_output_dir)
+    else:
+        raise ValueError('Invalid inputitemtype, must be file or dir')
+
+
+def perform_computation_on_files(self,
+                                 remote_workflow_file,
+                                 remote_input_dir,
+                                 remote_output_dir,
+                                 output_extension=''):
     self.update_state(state='PRESTAGING')
     remote_storage = remote_storage_client(app.config)
     input_dir = 'in'
     output_dir = 'out'
     local_input_dir, local_output_dir, session_dir = create_session_dir(input_dir, output_dir)
     workflow_file = fetch_workflow(remote_storage, session_dir, remote_workflow_file)
+
     input_files = fetch_input_files(remote_storage, local_input_dir, remote_input_dir)
     output_files = ['{0}{1}'.format(d, output_extension) for d in input_files]
-    job_order_file = write_job_order(session_dir, input_dir, input_files, output_files)
-    workflow_wrapper_file = write_workflow_wrapper(session_dir, workflow_file)
+    job_order_file = write_job_order_on_files(session_dir, input_dir, input_files, output_files)
+    workflow_wrapper_file = write_workflow_wrapper_on_files(session_dir, workflow_file)
 
     self.update_state(state='RUNNING')
     result = run_cwl(workflow_wrapper_file, job_order_file, session_dir, output_dir)
@@ -212,6 +263,31 @@ def perform_computation(self, remote_workflow_file, remote_input_dir, remote_out
 
     self.update_state(state='POSTSTAGING')
     upload_output_files(remote_storage, local_output_dir, output_files, remote_output_dir)
+    shutil.rmtree(session_dir)
+
+    result_url = remote_url(app.config, remote_output_dir)
+    result['url'] = result_url
+    return result
+
+
+def perform_computation_on_dirs(self, remote_workflow_file, remote_input_dir, remote_output_dir):
+    self.update_state(state='PRESTAGING')
+    remote_storage = remote_storage_client(app.config)
+    input_dir = 'in'
+    output_dir = 'out'
+    local_input_dir, local_output_dir, session_dir = create_session_dir(input_dir, output_dir)
+    workflow_file = fetch_workflow(remote_storage, session_dir, remote_workflow_file)
+
+    input_dirs = fetch_input_dirs(remote_storage, local_input_dir, remote_input_dir)
+    job_order_file = write_job_order_on_dirs(session_dir, input_dirs)
+    workflow_wrapper_file = write_workflow_wrapper_on_dirs(session_dir, workflow_file)
+
+    self.update_state(state='RUNNING')
+    result = run_cwl(workflow_wrapper_file, job_order_file, session_dir, output_dir)
+
+    self.update_state(state='POSTSTAGING')
+
+    upload_output_dir(remote_storage, local_output_dir, remote_output_dir)
     shutil.rmtree(session_dir)
 
     result_url = remote_url(app.config, remote_output_dir)
@@ -251,10 +327,10 @@ def fetch_input_files(remote_storage, local_input_dir, remote_input_dir):
     return input_files
 
 
-def fetch_workflow(beehub, local_input_dir, remote_workflow_file, workflow_file='workflow.cwl'):
+def fetch_workflow(remote_storage, local_input_dir, remote_workflow_file, workflow_file='workflow.cwl'):
     logging.warning('Downloading cwl')
     local_workflow_file = local_input_dir + '/' + workflow_file
-    beehub.download(remote_workflow_file, local_workflow_file)
+    remote_storage.download(remote_workflow_file, local_workflow_file)
     logging.warning('Validating cwl')
     document_loader, workflowobj, uri = fetch_document(local_workflow_file)
     validate_document(document_loader, workflowobj, uri)
@@ -282,6 +358,45 @@ def run_cwl(workflow_file, job_order_file, session_dir, output_dir):
     output_object = yaml.load(child_stdout)
 
     return {'exit_code': exit_code, 'log': child_stderr, 'output_object': output_object}
+
+
+def fetch_input_dirs(remote_storage, local_input_dir, remote_input_dir):
+    # recursively download files from remote_input_dir to local_input_dir
+    subdirs = set()
+    for input_file in remote_storage.ls(remote_input_dir, recursive=True):
+        remote_input_file = remote_input_dir + '/' + input_file
+        local_input_file = local_input_dir + '/' + input_file
+        dirname = os.path.dirname(local_input_file)
+        if dirname:
+            os.makedirs(dirname)
+            if '/' not in dirname:
+                subdirs.add(dirname)
+
+        logging.info('Downloading "' + remote_input_file + '" to "' + local_input_file + '"')
+        remote_storage.download(remote_input_file, local_input_file)
+
+    return subdirs
+
+
+def write_job_order_on_dirs(session_dir, input_dirs):
+    # TODO write a job order file
+    raise NotImplementedError()
+
+
+def write_workflow_wrapper_on_dirs(session_dir, workflow_file, workflow_wrapper_fn='workflow.wrapper.cwl'):
+    # TODO write a workflow wrapper
+    raise NotImplementedError()
+
+
+def upload_output_dir(remote_storage, local_output_dir, remote_output_dir):
+    for root, dirs, files in os.walk(local_output_dir):
+        remote_storage.mkdir(remote_output_dir + '/' + root)
+        for output_file in files:
+            local_output_file = local_output_dir + '/' + output_file
+            remote_output_file = remote_output_dir + '/' + output_file
+            logging.info('Uploading "' + local_output_file + '" to "' + remote_output_file + '"')
+            remote_storage.upload(local_output_file, remote_output_file)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', threaded=True)
